8261d4e791b897d225dcbd2696a49359fd4606e6
Jorge Revuelta Herrero <jorge.revuelta@bq.com>
2018-10-22 11:45:36 +0200

    Bump podspec, added on-demand resource download

--- Package.swift
+++ Package.swift
@@ -7,18 +7,19 @@ let package = Package(
     name: "POEditorParser",
     products: [
         .executable(name: "POEditorParser", targets: ["POEditorParser"]),
-	.executable(name: "poe", targets: ["POEditorParser"])
+	    .executable(name: "poe", targets: ["POEditorParser"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 	.package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
-	.package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0")
+	.package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
+    .package(url: "https://github.com/PerfectlySoft/Perfect-CURL", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "POEditorParser",
-            dependencies: ["Commander", "Rainbow"])
+            dependencies: ["Commander", "Rainbow", "PerfectCURL"])
     ]
 )
--- Sources/POEditorParser/main.swift
+++ Sources/POEditorParser/main.swift
@@ -1,34 +1,62 @@
 import Foundation
 import Commander
 import Rainbow
+import PerfectCURL
+
+let POEditorAPIURL = "https://api.poeditor.com/v2"
 
 let main = command(
-    Argument<String>("strings", description: "The input POEditor strings file directory."),
+    Argument<String>("APITOKEN", description: "The POEditor API token"),
+    Argument<Int>("id", description: "The id of the project"),
+    Argument<String>("language", description: "The language code"),
     Option<String>("swiftfile", default: "${SRCROOT}/${TARGET_NAME}/Literals.swift", description: "The output Swift file directory."),
     Option<String>("stringsfile", default: "${SRCROOT}/${TARGET_NAME}/Localizable.strings", description: "The output Strings file directory.")
-) { (strings: String, swiftfile: String, stringsfile: String) in
-    print("Reading contents of \(strings)...".blue)
-    let translationString = try! String(contentsOfFile: strings)
-    print("Parsing strings file...".blue)
-    let parser = StringTranslationParser(translation: translationString)
-    let translations = parser.parse()
+) { (token: String, id: Int, language: String, swiftfile: String, stringsfile: String) in
 
-    FileManager.default.createFile(atPath: swiftfile, contents: nil, attributes: nil)
-    guard let swiftHandle = FileHandle(forWritingAtPath: swiftfile) else {
-        print("Fatal error: Couldn't write to file located at \(swiftfile)".red)
-        return
+    print("Fetching contents of strings at POEditor...".blue)
+    
+    do {
+        let json = try CURLRequest("\(POEditorAPIURL)/projects/export", .failOnError,
+                                   .postField(.init(name: "api_token", value: token)),
+                                   .postField(.init(name: "id", value: "\(id)")),
+                                   .postField(.init(name: "language", value: language)),
+                                   .postField(.init(name: "type", value: "apple_strings")))
+            .perform()
+            .bodyJSON
+            
+        print("Querying POEditor for the latest strings file...".magenta)
+
+        if let result = json["result"] as? [String: Any], let url = result["url"] as? String {
+            print("Successfully got the latest URL for the strings file from POEditor".green)
+            print("Downloading the latest strings file from POEditor...".magenta)
+            let translationString = try CURLRequest(url, .failOnError)
+                .perform()
+                .bodyString
+            
+            print("Successfully downloaded the latest strings file from POEditor!".green)
+            print("Parsing strings file...".blue)
+            let parser = StringTranslationParser(translation: translationString)
+            let translations = parser.parse()
+            
+            FileManager.default.createFile(atPath: swiftfile, contents: nil, attributes: nil)
+            guard let swiftHandle = FileHandle(forWritingAtPath: swiftfile) else {
+                print("Fatal error: Couldn't write to file located at \(swiftfile)".red)
+                return
+            }
+            let fileCodeGenerator = FileCodeGenerator(fileHandle: swiftHandle)
+            fileCodeGenerator.generateCode(translations: translations)
+            print("Success! Literals generated at \(swiftfile)".green)
+            
+            FileManager.default.createFile(atPath: stringsfile, contents: nil, attributes: nil)
+            guard let stringsHandle = FileHandle(forWritingAtPath: stringsfile) else {
+                print("Fatal error: Couldn't write to file located at \(stringsfile)".red)
+                return
+            }
+            let stringsFileGenerator = StringsFileGenerator(fileHandle: stringsHandle)
+            stringsFileGenerator.generateCode(translations: translations)
+            print("Success! Strings generated at \(stringsfile)".green)
+        }
+    } catch let error {
+        print("Error found: \(error.localizedDescription)".red)
     }
-    let fileCodeGenerator = FileCodeGenerator(fileHandle: swiftHandle)
-    fileCodeGenerator.generateCode(translations: translations)
-    print("Success! Literals generated at \(swiftfile)".green)
-
-    FileManager.default.createFile(atPath: stringsfile, contents: nil, attributes: nil)
-    guard let stringsHandle = FileHandle(forWritingAtPath: stringsfile) else {
-        print("Fatal error: Couldn't write to file located at \(stringsfile)".red)
-        return
-    }
-    let stringsFileGenerator = StringsFileGenerator(fileHandle: stringsHandle)
-    stringsFileGenerator.generateCode(translations: translations)
-    print("Success! Strings generated at \(stringsfile)".green)
-
 }.run()
